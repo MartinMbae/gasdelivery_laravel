13c78eee5eba7c91284af1958e8041b417299c78
martin <martinmbae.codemaster@gmail.com>
2021-09-21 18:09:23 +0300

    Changed Ongoing to unpaid

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,1 +1,16 @@
-Password Update successful. You can navigate back to the app
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>AsapEnergies</title>
+
+</head>
+
+<body>
+<h4>Password Update successful.</h4>
+<p>You can navigate back to the app</p>
+</body>
+</html>
+
